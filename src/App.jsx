a24ad2b8e10237a772243262c4c524ac7e7bc7dc
StivenMastrovito <stivenmastrovito6@gmail.com>
2025-12-01 14:54:28 +0100

    feat: con bottoni aggiungi, submit, chiudi form, elimina oggetto, e messaggio di errore

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,37 @@ function App() {
 
   const [curArticolo, setCurArticolo] = useState("");
 
+  const [overview, setOverview] = useState("d-none");
+  const [errorMessageForm, setErrorMessageForm] = useState("d-none");
+
+
   return (
     <>
       <h1>LISTA ARTICOLI</h1>
       <ul>
         {listaArticoli.map((articolo, index) => (
-          <li key={index}>{listaArticoli[index]}</li>
+          <li key={index}>{listaArticoli[index]} <button className='delete' onClick={() => {
+            const copyListaArticoli = [...listaArticoli];
+            copyListaArticoli.splice(index, 1);
+            setListaArticoli(copyListaArticoli);
+          }}>ELIMINA</button></li>
         ))}
       </ul>
-      
+
+      <button className='add' onClick={() => { setOverview("d-flex") }}>AGGIUNGI UN ARTICOLO</button>
+
       {/* FORM */}
-      <form onSubmit={(event) => {
+      <form className={overview} onSubmit={(event) => {
         event.preventDefault();
-        const copyListaArticoli = [curArticolo, ...listaArticoli];
-        setListaArticoli(copyListaArticoli);
-        setCurArticolo("");
-        console.log(listaArticoli);
+        if (curArticolo.length > 2) {
+          const copyListaArticoli = [curArticolo, ...listaArticoli];
+          setListaArticoli(copyListaArticoli);
+          setCurArticolo("");
+          setOverview("d-none")
+          setErrorMessageForm("d-none");
+        } else {
+          setErrorMessageForm("d-block");
+        }
 
       }}>
         <div className='input'>
@@ -31,7 +46,15 @@ function App() {
           <input type="text" placeholder='Inserisci un articolo...' value={curArticolo} onChange={(event) => {
             setCurArticolo(event.target.value);
           }} />
+          <p className={`errorMessage ${errorMessageForm}`}>INSERISCI ALMENO 3 CARATTERI</p>
           <button className='submit' type='submit'>AGGIUNGI</button>
+          <button className='close' type='button' onClick={() => {
+            console.log("chiudi");
+            
+            setCurArticolo("");
+            setOverview("d-none")
+            setErrorMessageForm("d-none");
+          }}>X</button>
         </div>
       </form>
     </>
